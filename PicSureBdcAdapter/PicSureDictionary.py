0b6cf567456dde787bb39645d31877f76e11ff3f
Ryan Amari <ramari16@gmail.com>
2024-07-24 15:14:51 -0400

    ALS-6279: Update genomic annotations request to use correct endpoint

--- PicSureBdcAdapter/PicSureDictionary.py
+++ PicSureBdcAdapter/PicSureDictionary.py
@@ -32,7 +32,7 @@ class PicSureDictionary:
 
     def genotype_annotations(self):
         queryJSON = {"query":{"expectedResultType": "INFO_COLUMN_LISTING"}}
-        httpResults = self._apiObj.syncQuery(self._resourceUUID, json.dumps(queryJSON))
+        httpResults = self._apiObj.syncQuery(self.resourceUUID, json.dumps(queryJSON))
         result = json.loads(httpResults)
         return result
 
